5a353e3c2534341ed35fb8904084d2d8c14be125
josuezuber1994 <99942169+josuezuber1994@users.noreply.github.com>
2022-07-16 14:53:45 -0500

    Update ej19.java

--- josue/ej19.java
+++ josue/ej19.java
@@ -10,6 +10,9 @@ import javax.swing.JOptionPane;
 
 public class ej19 {
     public static void main(String[] args) {
+        //Escriba un algoritmo que reciba una fecha (día y mes) correspondiente al año
+       //2014 e imprima por pantalla el número de días que han pasado desde el 1 de
+       //Enero de 2014 hasta la fecha dada.
         LocalDate date = LocalDate.of(2014,1,1);
         int mes = date.getMonthValue(), dia = date.getDayOfMonth();
         int mesIn, diaIn;
