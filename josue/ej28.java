c4cc16453984dc79eabe2b72968ac8ba6066cd15
josuezuber1994 <99942169+josuezuber1994@users.noreply.github.com>
2022-07-16 15:22:09 -0500

    Update ej28.java

--- josue/ej28.java
+++ josue/ej28.java
@@ -11,6 +11,15 @@ public class ej28 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+          //ejercicio
+        //Se tiene una secuencia de enteros terminada en cero, que corresponde a la edad,
+        //peso y estatura de una muestra de hombres y mujeres mayores de 18 años. Con
+        // base en dicha secuencia se desea realizar un estudio a fin de conocer:
+        //Edad promedio de todas las personas en la muestra.
+        //Peso promedio de todas las personas en la muestra.
+        //Estatura promedio de todas las personas en la muestra.
+       //Cuántas personas hay con edad entre los 18 y 25 años.
+      //Cuántas personas son mayores a 36 años.
 
         int cEdad = 0, cEdad2 = 0;
         double edad = 0, peso = 0, estatura = 0, pEdad = 0, pEstatura = 0, pPeso = 0, pPesoEdad = 0;
