7a995917a85f67cb191f6e4ee94d3b46455699c6
josuezuber1994 <99942169+josuezuber1994@users.noreply.github.com>
2022-07-16 14:51:13 -0500

    Update ej18.java

--- josue/ej18.java
+++ josue/ej18.java
@@ -8,6 +8,22 @@ import java.util.Scanner;
 
 public class ej18 {
 public static void main(String[] args) {
+        //ejercicio 18
+        //El IMC resulta de la división de la masa del individuo (en kilogramos) entre el
+        //cuadrado de la estatura (en metros). El índice de masa corporal es un indicador
+        //del peso de una persona en relación con su altura.
+       // Clasificación del IMC de acuerdo con la OMS de la ONU:
+        //a. Menor a 16: Criterio de ingreso.
+        //b. 16 a 16.9: infra peso.
+        //c. 17 a 18.4: bajo peso.
+        //d. 18.5 a 24.9: peso normal.
+        //e. 25 a 29.9: sobrepeso.
+        //f. 30 a 34.9: obesidad pre-mórbida.
+        //g. 40 a 45: obesidad mórbida.
+        //h. Mayor a 45: obesidad híper-mórbida.
+        //Dado el peso de una persona en libras (1 libra = 0,453592 Kg) y su estatura en
+        //centímetros, calcule su IMC e indique como salida el peso en kilogramos, el valor
+        //de IMC de la persona y la categoría en la cual fue clasificado.
         Scanner in = new Scanner(System.in);
         double kg, lb, et, mt, imc;
         String cl= "";
