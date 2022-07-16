e78786dd765a84d336589b05315da5ec585fe575
josuezuber1994 <99942169+josuezuber1994@users.noreply.github.com>
2022-07-16 15:25:41 -0500

    Update ej29.java

--- josue/ej29.java
+++ josue/ej29.java
@@ -9,6 +9,9 @@ import java.util.Scanner;
 public class ej29 {
     public static void main(String[] args) {
 Scanner input = new Scanner(System.in);
+        //ejercicio
+        //Construye un algoritmo que calcule e imprima la tabla de multiplicar, desde la tabla
+       // del 1 hasta la del 10.
         int numero = 0;
         for (int indice = 1; indice<= 10; indice++) {
             for (int j = 1; j <= 10; j++) {
