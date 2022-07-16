7470ef98582d21d9c349be56066a358c251a1975
josuezuber1994 <99942169+josuezuber1994@users.noreply.github.com>
2022-07-16 15:34:36 -0500

    Update ej26.java

--- josue/ej26.java
+++ josue/ej26.java
@@ -12,11 +12,11 @@ public class ej26 {
 
         Scanner input = new Scanner(System.in);
         //Ejercicio
-        //Dado un número entero N que representa una contraseña y asumiendo que una
-        //contraseña debe tener al menos 10 dígitos para ser segura, determine si la
-        //contraseña ingresada por el usuario es correcta, de no serlo debe pedirla
-        //nuevamente hasta que tenga los 10 (diez) dígitos solicitados y al ser correcta
-        //mostrar un mensaje de éxito al usuario, por salida estándar.
+      //Dado un número entero N que representa una contraseña y asumiendo que una
+      //contraseña debe tener al menos 10 dígitos para ser segura, determine si la
+      //contraseña ingresada por el usuario es correcta, de no serlo debe pedirla
+      //nuevamente hasta que tenga los 10 (diez) dígitos solicitados y al ser correcta
+      //mostrar un mensaje de éxito al usuario, por salida estándar
         int numero = 0;
         String palabra = "";
         System.out.println("ingrese la contraseña");
