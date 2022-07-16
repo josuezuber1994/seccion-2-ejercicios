86d4472ada68a3a60bdd8603fe9f0036a7296718
josuezuber1994 <99942169+josuezuber1994@users.noreply.github.com>
2022-07-16 14:09:17 -0500

    Update ej16.java

--- josue/ej16.java
+++ josue/ej16.java
@@ -9,6 +9,16 @@ import java.util.Scanner;
 public class ej16 {
 
     public static void main(String[] args) {
+         //ejercicio 16
+        //En un estacionamiento el monto a pagar se calcula multiplicando el número de
+        //horas que permaneció el automóvil dentro del estacionamiento por Bs. 4 y se
+        //incrementa esta cantidad en Bs. 2,50 por cada media hora adicional.
+        //Ahora se desea que usted elabore un algoritmo que a partir de la hora de entrada
+        //y la hora de salida de un vehículo (las mismas corresponden a un mismo día)
+        //calcule el monto a pagar por el dueño del vehículo.
+        //La entrada vendrá dada por dos enteros positivos el primero representa las horas
+        //y el segundo los minutos, además por último se debe leer un carácter (A o P) que
+        //indica si la hora es AM o PM.
         double x = 0.0, a = 0.0, b = 0.0, c = 0.0;
         int numero1 = 0, numero2 = 0, numero3 = 0;
         Scanner input = new Scanner(System.in);
