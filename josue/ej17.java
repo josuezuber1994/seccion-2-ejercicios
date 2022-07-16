55841aaef0838bd7ee8a42b9894240cb9e94d5c4
josuezuber1994 <99942169+josuezuber1994@users.noreply.github.com>
2022-07-16 14:38:27 -0500

    Update ej17.java

--- josue/ej17.java
+++ josue/ej17.java
@@ -10,25 +10,19 @@ public class ej17 {
      static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         //ejercicio 17 
-        //El IMC resulta de la división de la masa del individuo (en kilogramos) entre el
-        //cuadrado de la estatura (en metros). El índice de masa corporal es un indicador
-        //del peso de una persona en relación con su altura.
-        //Clasificación del IMC de acuerdo con la OMS de la ONU:
-        //a. Menor a 16: Criterio de ingreso.
-        //b. 16 a 16.9: infra peso.
-        //c. 17 a 18.4: bajo peso.
-        //d. 18.5 a 24.9: peso normal.
-        //e. 25 a 29.9: sobrepeso.
-        //f. 30 a 34.9: obesidad pre-mórbida.
-        //g. 40 a 45: obesidad mórbida.
-        //h. Mayor a 45: obesidad híper-mórbida.
-        //Dado el peso de una persona en libras (1 libra = 0,453592 Kg) y su estatura en
-        //centímetros, calcule su IMC e indique como salida el peso en kilogramos, el valor
-        //de IMC de la persona y la categoría en la cual fue clasificado.
+        //En un estacionamiento el monto a pagar se calcula multiplicando el número de
+       //horas que permaneció el automóvil dentro del estacionamiento por Bs. 4 y se
+       //incrementa esta cantidad en Bs. 2,50 por cada media hora adicional.
+       //Ahora se desea que usted elabore un algoritmo que a partir de la hora de entrada
+       //y la hora de salida de un vehículo (las mismas corresponden a un mismo día)
+     //calcule el monto a pagar por el dueño del vehículo.
+     //La entrada vendrá dada por dos enteros positivos el primero representa las horas
+    //y el segundo los minutos, además por último se debe leer un carácter (A o P) que
+     //indica si la hora es AM o PM
         int hora;
         System.out.print("Ingrese la cantidad de horas: ");
         hora = input.nextInt();
-        System.out.println("El recio es: " + cPrecio(hora));
+        System.out.println("El precio es: " + cPrecio(hora));
     }
     public static double cPrecio(int hora) {
         double precio = 0;
